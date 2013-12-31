f2cf931ef1ae236f7ef454a45e6b75a0cfd222d2
Ivar in 't Veen <ivarveen@gmail.com>
2013-12-31 13:03:57 +0100

    Added part of lookahead heuristic

--- minisat/core/Solver.h
+++ minisat/core/Solver.h
@@ -152,6 +152,7 @@ public:
     double    var_decay;
     double    clause_decay;
     double    random_var_freq;
+    double    activity_look_freq;
     double    activity_nl_freq;
     double    sym_count_freq;
     double    random_seed;
@@ -177,7 +178,7 @@ public:
 
     // Statistics: (read-only member variable)
     //
-    uint64_t heur_act_nl_usages,heur_rand_usages, heur_original_sp_usages, heur_sym_count_usages;
+    uint64_t heur_act_look_usages,heur_act_nl_usages,heur_rand_usages, heur_original_sp_usages, heur_sym_count_usages;
     uint64_t solves, starts, decisions, rnd_decisions, propagations, conflicts;
 	uint64_t dec_vars, num_clauses, num_learnts, clauses_literals, learnts_literals, max_literals, tot_literals;
     uint64_t sympropagations, symconflicts, invertingSyms;
